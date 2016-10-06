a9b6e86e2f06666568abb9f48c60656821d19569
manudurgoni <durgoni.emmanuel@live.fr>
2016-10-06 13:22:44 +0200

    Update renderer.js

--- renderer.js
+++ renderer.js
@@ -1,3 +1,8 @@
 // This file is required by the index.html file and will
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
+
+//Set a "normal" css float number !
+document.body.style.height = "1025.48px";
+
+//The result in app is : style="height: 1025,48px;"
